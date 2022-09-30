4b99feb20573db1c105ef261ba9562811df051cd
ZaryabAlam <zaryabalam97@gmail.com>
2022-10-01 00:36:18 +0500

    Work Done

---
+++ lib/first.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class First extends StatefulWidget {
+  @override
+  State<First> createState() => _FirstState();
+}
+
+class _FirstState extends State<First> {
+  @override
+  Widget build(BuildContext context) {
+    final _h = MediaQuery.of(context).size.height;
+    final _w = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: _h * 0.2),
+                Center(
+                  child: Container(
+                    height: _h * 0.3,
+                    width: _w * 0.4,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://i.ibb.co/sR6MLLm/b-logo3.png",
+                            ),
+                            fit: BoxFit.contain)),
+                  ),
+                ),
+                Text(
+                  "Mockup Zong 4G clone application",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w100),
+                ),
+                SizedBox(height: _h * 0.1),
+                Container(
+                  height: _h * 0.07,
+                  width: _w * 0.9,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: Colors.cyan),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Lets begin"),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        shadowColor: Colors.transparent),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: _h * 0.2,
+                  width: _w * 0.5,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                            "https://i.ibb.co/kJrJX1v/b-logo4.png",
+                          ),
+                          fit: BoxFit.contain)),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bong/first.dart';
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        body: First(),
+      ),
+    );
+  }
+}
